a669f5d721108912260f30b34fc01fbb81087ccc
Unknown <jakash1997@gmail.com>
2019-09-24 21:26:18 +0530

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
-import styled from "styled-components";
+import styled from "styled-components"
 
+import "./index.css"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const Background = styled.div`
-position: fixed; z-index: -99; width: 100%; height: 100%
-`
+const primaryColor = "white";
 
 const IndexPage = () => (
   <div style={{ textAlign: "center" }}>
-    <Background>
-      <iframe
-        frameborder="0"
-        height="100%"
-        width="100%"
-        src="https://www.youtube.com/embed/F6Fr0Ip38pA?autoplay=1&controls=0&showinfo=0&autohide=1&mute=1"
-      ></iframe>
-    </Background>
+      <video id="videoBG" poster="poster.JPG" autoPlay muted loop>
+        <source src="title-video.m4v" />
+      </video>
     <SEO title="Home" />
-    <h1 style={{color:'white'}}>Blögomir - One Blog to Rule Them All</h1>
-    <p style={{color:'white'}}>Own your own Content</p>
-    <Link style={{color:'white'}} to="/create">Get Started with your first import</Link>
+    <h1 style={{color:primaryColor, paddingTop:"40vh"}}>Blögomir - One Blog to Rule Them All</h1>
+    <p style={{color:primaryColor}}>Own your own Content</p>
+    <Link style={{color:primaryColor}} to="/create">Get Started with your first import</Link>
   </div>
 )
 
